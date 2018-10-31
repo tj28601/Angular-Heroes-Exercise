b873702cd34229fefa82012f17a829be070dc807
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-10-30 23:52:03 -0400

    pushing heroes Angular exercise to GitHub

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,32 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Lebron James' },
+  { id: 12, name: 'Kobe Bryant' },
+  { id: 13, name: 'Kareem' },
+  { id: 14, name: 'JR Smith' },
+  { id: 15, name: 'Kyrie Irving' },
+  { id: 16, name: 'Dirk Nowitzki' },
+  { id: 17, name: 'Manu Ginobili' },
+  { id: 18, name: 'Kawhi Leonard' },
+  { id: 19, name: 'DeMar DeRozan' },
+  { id: 20, name: 'Steph Curry' }
+];
+
+
+
+
+// import { Hero } from './hero';
+//
+// export const HEROES: Hero[] = [
+//   { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
