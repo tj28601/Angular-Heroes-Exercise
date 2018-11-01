f7bfd0d93e441460abbf2fa1333f7d2057f8b001
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-10-31 21:00:36 -0400

    practiced using services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]> {
+  //To do: send the message _after_fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
 }
