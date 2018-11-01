138909712338306e7535f4051878ec3d26a8d077
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-11-01 16:45:24 -0400

    added routing. now working on HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,38 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms'; //<-- NgModel lives here
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+
+    // HttpClientInMemoryWebApiModule intercepts HTTP requests and returns simulated responses....Remove when a real server is ready to receive requests.
+
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
